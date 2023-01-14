78966d9ed4bd173c3a11d49ba498a41dc092cf0c
RedsonJunior <r.junior.editor@gmail.com>
2023-01-13 23:39:42 -0300

    teste-02

--- teste02.js
+++ teste02.js
@@ -1,15 +1,11 @@
-const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
-const salaJava = [6, 5, 8, 9, 5, 6];
-const salaPython = [7, 3.5, 8, 9.5];
+const notas = [10, 6.5, 8, 7.5];
 
-function calculaMedia(notasDaSala) {
-  const somaDasNotas = notasDaSala.reduce((acc, nota) => acc + nota, 0);
+let somaDasNotas = 0;
 
-  const media = somaDasNotas / notasDaSala.length;
-
-  return media;
+for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
 }
 
-console.log(`A média da sala de JavaScript é ${calculaMedia(salaJS)}`);
-console.log(`A média da sala de Java é ${calculaMedia(salaJava)}`);
-console.log(`A média da sala de Python é ${calculaMedia(salaPython)}`); 
+const media = somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}`)
